d88efa2a745087bdf027dd26828767f62e21d01a
Matheus Paiva <matheus.paiva@hexis-hub.com>
2019-03-27 01:58:23 +0000

    Update example

--- example/src/store/models/todos.js
+++ example/src/store/models/todos.js
@@ -1,4 +1,4 @@
-import createModelList from 'rematch-model-list';
+import createModelList from '@gapless-tech/rematch-model-list';
 import * as api from '../../todos-api-service';
 
 export default createModelList('todos', api);
